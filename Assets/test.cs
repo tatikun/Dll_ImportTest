27c940e2752b334c53797520b740ace13313c472
TTTT <40955281+tatikun@users.noreply.github.com>
2020-05-07 23:32:49 +0900

    FirstCommit

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class test : MonoBehaviour
+{
+    [DllImport("DllToUnity")]
+    private static extern int counter();
+
+    [DllImport("DllToUnity")]
+    private static extern void resetCount();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetCount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(counter());
+    }
+}
